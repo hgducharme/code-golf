595176c9788d734177aba29b4aca9f920504337f
Hunter Ducharme <hgducharme@aol.com>
2014-09-14 22:41:49 -0500

    Further progression on sketch-pad / updates to javascript file

--- webDev101/javascriptSketchPad/sketch-pad.js
+++ webDev101/javascriptSketchPad/sketch-pad.js
@@ -1,25 +1,40 @@
+$(function() {
+  buildTable(16);
+  hover();
+});
+
+
+
 // Creates Table
-var sketch = {
-	addTable: function() {
-		$(".grid").append('<table><tbody></tbody></table>');
-	},
+function buildTable(n) {
+  var b
+  $('.grid').append('<table><tbody></table></tbody>');
 
-	addRows: function() {
-		for (var i = 0; i < 16; i++) {
-			$("tbody").append('<tr></tr>');
-		}
-	},
+  for(var i = 0; i < n; i++) {
+    $('tbody').append('<tr></tr>')
+  };
 
-  	addCells: function() {
-		for (var i = 0; i < 16; i++) {
-			$("tr").append("<td></td>");
-		}
-	},
+  for(var i = 0; i < n; i++) {
+    $('tr').append('<td></td>')
+  };
 };
 
-$(document).ready(function() {
-    sketch.addTable();
-    sketch.addRows();
-    sketch.addCells();
-   }
-);
+
+
+// Hover Function To Add Colors
+function hover() {
+  $('td').hover(function() {
+    $(this).css('background-color', 'green');
+  },
+
+  function() {
+    $(this).css('background-color', 'green');
+  });
+};
+
+// Prompt Question & Re-build Table
+/* function prompt() {
+  $('.btn').click(function() {
+    prompt("How many rows and columns would you like? Pick a number between (1 & 64)", "16");
+  });
+} */
