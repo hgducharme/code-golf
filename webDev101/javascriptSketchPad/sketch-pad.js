3b86d0a4d99b5875af286dcb146a3de0ef1aed56
Hunter Ducharme <hgducharme@aol.com>
2014-09-15 19:51:37 -0500

    Added randomColor mode, trail mode, and normal mode, & changed color theme of sketch pad

--- webDev101/javascriptSketchPad/sketch-pad.js
+++ webDev101/javascriptSketchPad/sketch-pad.js
@@ -1,13 +1,18 @@
 $(function() {
   buildTable(16);
   hover();
+  resetTable();
+  trail();
+  randomColorHover();
 });
 
 
 
 // Creates Table
 function buildTable(n) {
-  var b
+
+  var boxSize = (850/n);
+
   $('.grid').append('<table><tbody></table></tbody>');
 
   for(var i = 0; i < n; i++) {
@@ -15,7 +20,9 @@ function buildTable(n) {
   };
 
   for(var i = 0; i < n; i++) {
-    $('tr').append('<td></td>')
+    $('tr').append('<td></td>');
+    $('td').css('height', boxSize);
+    $('td').css('width', boxSize);
   };
 };
 
@@ -24,17 +31,66 @@ function buildTable(n) {
 // Hover Function To Add Colors
 function hover() {
   $('td').hover(function() {
-    $(this).css('background-color', 'green');
+    $(this).css('background-color', 'rgb(90,188,215)');
   },
 
   function() {
-    $(this).css('background-color', 'green');
+    $(this).css('background-color', 'rgb(90,188,215)');
   });
 };
 
-// Prompt Question & Re-build Table
-/* function prompt() {
-  $('.btn').click(function() {
-    prompt("How many rows and columns would you like? Pick a number between (1 & 64)", "16");
+
+// Rebuilds Table When Button Is Clicked
+function resetTable() {
+  $('button').on('click', function() {
+
+    var choice = prompt("How many columns and rows would you like? Please pick a number between (1 & 64).", "16");
+    
+    if (choice < 1 || choice > 64) {
+      alert("That was not a valid entry, please specify a number between (1 & 64)");
+      return null;
+    }
+
+    else {
+      var resetColor = $('td').css('background-color', 'rgb(215,215,215)');
+      var removeTable = $('table').remove();
+      buildTable(choice);
+      hover();
+    }
   });
-} */
+}
+
+
+
+// Creates Trail
+function trail() {
+  $("#trail").click(function() {
+    $('td').hover(function() {
+      $(this).fadeIn('fast');
+    },
+
+    function() {
+      $(this).fadeOut('fast');
+    });
+  });
+};
+
+
+// Creates Random Color Mode
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + "," +  g + ","+ b + ")"
+};
+
+function randomColorHover() {
+  $('#randomColor').click(function() {
+    $('td').hover(function() {
+      $(this).css('background-color', randomColor());
+    }, function() {
+      $(this).css('background-color', randomColor());
+    });
+  });
+}
