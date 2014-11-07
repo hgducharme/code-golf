fbd50be892dc201c4f3a7a16440663f3e66a8bf7
Hunter Ducharme <hgducharme@gmail.com>
2014-11-07 00:28:17 -0600

    Added update method

--- webDev101/RailsBlogger/app/controllers/articles_controller.rb
+++ webDev101/RailsBlogger/app/controllers/articles_controller.rb
@@ -23,6 +23,13 @@ include ArticlesHelper
 		@article = Article.find(params[:id])
 	end
 
+	def update
+		@article = Article.find(params[:id])
+  	@article.update(article_params)
+
+  	redirect_to article_path(@article)
+	end
+
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
