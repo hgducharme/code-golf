4ec75529f306be82bf48e45401e6afdb19a6bc14
Hunter Ducharme <hgducharme@gmail.com>
2014-11-07 00:25:46 -0600

    Added edit method

--- webDev101/RailsBlogger/app/controllers/articles_controller.rb
+++ webDev101/RailsBlogger/app/controllers/articles_controller.rb
@@ -19,6 +19,10 @@ include ArticlesHelper
   	redirect_to article_path(@article)
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def destroy
 		@article = Article.find(params[:id])
 		@article.destroy
