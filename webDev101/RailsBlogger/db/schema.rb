235e3a07a5266272ff61f373b5b4187254bb1158
Hunter Ducharme <hgducharme@gmail.com>
2014-11-07 11:49:34 -0600

    Migrated DB for comment model

--- webDev101/RailsBlogger/db/schema.rb
+++ webDev101/RailsBlogger/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107035250) do
+ActiveRecord::Schema.define(version: 20141107174315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,15 @@ ActiveRecord::Schema.define(version: 20141107035250) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
 end
