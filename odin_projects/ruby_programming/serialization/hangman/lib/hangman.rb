6fb782398b612f775c9f2b759c0d8afdcf5d657f
Hunter Ducharme <hgducharme@gmail.com>
2015-01-13 23:03:10 -0600

    added basic structure

--- odin_projects/ruby_programming/serialization/hangman/lib/hangman.rb
+++ odin_projects/ruby_programming/serialization/hangman/lib/hangman.rb
@@ -1,5 +1,19 @@
-require "hangman/version"
+# require "hangman/version"
+require 'csv'
 
 module Hangman
-  # Your code goes here...
-end
+  class Game
+    def initialize()
+      puts 
+      puts "Welcome to CLI hangman! Can you beat the computer?"
+      new_game
+    end
+
+    def new_game
+      dictionary = File.read('hangman/5desk.txt')
+      puts dictionary[0...5]
+    end
+  end # Class - Hangman
+end # Module - Hangman
+
+game = Hangman::Game.new
